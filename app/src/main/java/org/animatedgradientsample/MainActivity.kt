2e20d5da815fcfc1a16a42d22f9f3225752e731d
dhananjay-m2 <dhananjay.mohnot@aidash.com>
2022-01-20 16:44:47 +0530

    Animated gradient working fine.

--- app/src/main/java/org/animatedgradientsample/MainActivity.kt
+++ app/src/main/java/org/animatedgradientsample/MainActivity.kt
@@ -1,11 +1,19 @@
 package org.animatedgradientsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rootLayout = findViewById<ConstraintLayout>(R.id.rootLayout)
+        val animationDrawable = rootLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(1500);
+        animationDrawable.setExitFadeDuration(3000);
+        animationDrawable.start()
     }
 }
